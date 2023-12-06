964648bcbb52b3662380e202d534cb5741635beb
Cauã Moreno <caua.moreno@estudante.ufjf.br>
2023-12-05 21:44:06 -0300

    adiciona função 'atualizaTela'

--- estoque.js
+++ estoque.js
@@ -1,1 +1,16 @@
-let estoque = {};
+let estoque = {
+    'joao': [
+        {'tipo': 'maca', 'qtd': 1},
+        {'tipo': 'pera', 'qtd': 2}
+    ],
+    'maria': [
+        {'tipo': 'maca', 'qtd': 2},
+        {'tipo': 'banana', 'qtd': 4}
+    ]
+};
+
+function getEstoque(){
+    return structuredClone(estoque);
+}
+
+export { getEstoque };
